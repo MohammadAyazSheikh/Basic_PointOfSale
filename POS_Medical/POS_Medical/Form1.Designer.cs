5fedc248d7513a477fb3038ea11ad7e6b2e612fd
Mohammad Ayaz <mohammadayazsheikh@gmail.com>
2020-01-14 14:48:49 +0500

    1st commit

---
+++ POS_Medical/POS_Medical/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace POS_Medical
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelContainer.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelContainer.Location = new System.Drawing.Point(0, 1);
+            this.panelContainer.Margin = new System.Windows.Forms.Padding(0);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(70, 660);
+            this.panelContainer.TabIndex = 0;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Location = new System.Drawing.Point(70, 1);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(0);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(52, 40);
+            this.btnMenu.TabIndex = 1;
+            this.btnMenu.Text = "button1";
+            this.btnMenu.UseVisualStyleBackColor = true;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.panelContainer);
+            this.Name = "MainForm";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Button btnMenu;
+    }
+}
+
---
+++ POS_Medical/POS_Medical/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_Medical
+{
+    public partial class MainForm : Form
+    {
+        Timer timer;
+
+        int panelW = 150;
+        int panelH = 660;
+        public MainForm()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            //panelContainer.Size = new Size(660,150);
+            panelContainer.Width = panelW;
+            btnMenu.Location = new Point(panelContainer.Width, btnMenu.Location.Y);
+        }
+    }
+}
